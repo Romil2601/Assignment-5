d91c2f7f96165b8825c6b0f12c103f94637942e5
Romil <romil.raja@bwispl.in>
2025-09-17 10:41:00 +0530

    4th done

--- 4.py
+++ 4.py
@@ -35,3 +35,12 @@ else:
 
 
 # 4. Practical Example 8: Write a Python program to check if a person is eligible to donate blood using a nested if.
+age = int(input("Enter your age: "))
+weight = float(input("Enter your weight in kg: "))
+if age >= 18:
+    if weight >= 50:
+        print("You are eligible to donate blood.")
+    else:
+        print("You must weigh at least 50 kg to donate blood.")
+else:
+    print("You must be at least 18 years old to donate blood.")
